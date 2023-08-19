929c66bb6fd93d1cb405fa73e157d63992ce376b
robby <5150reb@gmail.com>
2023-08-18 23:00:55 -0400

    declared variables for buttons in js
    declared number and operation buttons by id in javascript
    temporarily changed background to gray bc its dark rn and it hurts my eyes if it's white

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+//variables for operation functions
 let firstNumber = undefined;
 let secondNumber = undefined;
 let operator = undefined;
 
+//declare number buttons
+const zeroButton = document.getElementById('zero');
+const oneButton = document.getElementById('one');
+const twoButton = document.getElementById('two');
+const threeButton = document.getElementById('three');
+const fourButton = document.getElementById('four');
+const fiveButton = document.getElementById('five');
+const sixButton = document.getElementById('six');
+const sevenButton = document.getElementById('seven');
+const eightButton = document.getElementById('eight');
+const nineButton = document.getElementById('nine');
 
-//define functions
+//declare operation buttons
+const addButton = document.getElementById('add');
+const subtractButton = document.getElementById('subtract');
+const multiplyButton = document.getElementById('multiply');
+const divideButton = document.getElementById('divide');
+
+//declare clear and equals buttons
+const clearButton = document.getElementById('clear');
+const equalButton = document.getElementById('equal');
+
+//calculator display
+const display = document.querySelector('.display');
+let displayText = display.textContent;
+
+
+//define operation functions
 
 function operate(firstNumber, secondNumber, operator) {
     if (operator === "+") {
@@ -39,3 +66,6 @@ function divide(firstNumber, secondNumber) {
     return result = firstNumber / secondNumber;
 
 };
+
+//define button-to-display functions
+
